fe62de096947f2bfcd33c05e942253ac7505e08e
Marco Martinez <marco.antoni.martinez@gmail.com>
2025-06-17 18:51:16 +0000

    Added files from following along the running real programs lesson

---
+++ howdy.rb
@@ -0,0 +1,19 @@
+
+pp "Hello world!"
+
+require "active_support/all"
+require "./goodbye.rb"
+
+4398.ordinalize
+
+pp "octopus".pluralize
+
+pp "ies".singularize
+
+pp "pies".singularize
+
+pp "What's your name?"
+
+their_name = gets.chomp
+
+puts "Hello, " + their_name + "!"
